f7de9515a553be9a31af99f8aa206124048ea19e
Fzal <suhailfzl@gmail.com>
2021-02-28 16:11:00 +0530

    initial commit

---
+++ findsig.py
@@ -0,0 +1,58 @@
+import re
+
+def find_mortifs(line):
+    pat1 = 'AATAACAA'
+    pat2 = 'AWWRTAANNWWGNCC'
+    mortifs = []
+    char8 = [line[i:i+8] for i in range(0, len(line), 8)]
+    char16 = [line[i:i+16] for i in range(0, len(line), 16)]
+    for s in char8:
+        mortifs += [ (s[m.start():m.end()], m.start()) for m in re.finditer(pat1, s) ]
+    for s in char16:
+        mortifs += [ (s[m.start():m.end()], m.start()) for m in re.finditer(pat2, s) ]
+    return mortifs
+
+def find_oris(line):
+    patterns = ['ATTA', 'ATTT', 'ATTTTA']
+    oris = []
+    for pattern in patterns:
+        oris += [ (line[m.start():m.end()], m.start(), m.end()) for m in re.finditer(pattern, line) ]
+    return oris
+
+def find_tgs(line):
+    patterns = ['TGTTTTG', 'TTTTGGGG', 'TGTTTTTG']
+    tgs = []
+    for pattern in patterns:
+        tgs += [ (line[m.start():m.end()], m.start()) for m in re.finditer(pattern, line) ]
+    return tgs
+
+
+
+
+def findsig(filename):
+    fh = open(filename)
+    b_mof = []
+    oris = []
+    tgs = []
+    lines = fh.readlines()
+    print(lines)
+    for line in lines:
+        mortifs = find_mortifs(line)
+        if mortifs:
+            b_mof.append(mortifs)
+        ori = find_oris(line)
+        if ori:
+            oris.append(ori)
+        tg = find_tgs(line)
+        if tg:
+            tgs.append(tg)
+
+    print('b_mof')
+    print(b_mof)
+    print('ori')
+    print(oris)
+    print('tg')
+    print(tgs)
+
+
+findsig('input.txt')
